63d9a634de154e1bf022857962fa3a80338d315a
briggsrr <robert.briggs.3@icloud.com>
2022-10-26 17:37:29 -0700

    bb - Reverting to POST state

--- src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
@@ -82,36 +82,5 @@ public class MenuItemReviewController extends ApiController {
         return savedMenuItemReview;
     }
 
-    @ApiOperation(value = "Delete a menu item review")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @DeleteMapping("")
-    public Object deleteUCSBDate(
-            @ApiParam("id") @RequestParam Long id) {
-        MenuItemReview menuItemReview = menuItemReviewRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(MenuItemReview.class, id));
 
-        menuItemReviewRepository.delete(menuItemReview);
-        return genericMessage("MenuItemReview with id %s deleted".formatted(id));
-    }
-
-    @ApiOperation(value = "Update a menu item review")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @PutMapping("")
-    public MenuItemReview updateMenuItemReview(
-            @ApiParam("id") @RequestParam Long id,
-            @RequestBody @Valid MenuItemReview  incoming) {
-
-        MenuItemReview menuItemReview = menuItemReviewRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(MenuItemReview.class, id));
-
-        menuItemReview.setItemId(incoming.getItemId());
-        menuItemReview.setReviewerEmail(incoming.getReviewerEmail());
-        menuItemReview.setStars(incoming.getStars());
-        menuItemReview.setDateReviewed(incoming.getDateReviewed());
-        menuItemReview.setComments(incoming.getComments());
-
-        menuItemReviewRepository.save(menuItemReview);
-
-        return menuItemReview;
-    }
 }
