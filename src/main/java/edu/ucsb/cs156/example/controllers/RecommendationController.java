d6f3c6d9e4ede4324fa7f1106e058d9707283a3d
Alexander Robles <alexanderrobles@umail.ucsb.edu>
2022-10-27 17:21:55 -0700

    ar - added test files and began creating tests for rec controller

---
+++ src/main/java/edu/ucsb/cs156/example/controllers/RecommendationController.java
@@ -0,0 +1,45 @@
+package main.java.edu.ucsb.cs156.example.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.ucsb.cs156.example.controllers.ApiController;
+import edu.ucsb.cs156.example.errors.EntityNotFoundException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+import main.java.edu.ucsb.cs156.example.entities.Recommendation;
+import main.java.edu.ucsb.cs156.example.repositories.RecommendationRepository;
+@Api(description = "recommendations")
+@RequestMapping("/api/recommendations")
+@RestController
+@Slf4j
+public class RecommendationController extends ApiController {
+
+    @Autowired
+    RecommendationRepository recommendationRepository;
+
+    @ApiOperation(value = "List all recommendations")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/all")
+    public Iterable<Recommendation> allRecommendations() {
+        Iterable<Recommendation> recommendations = recommendationRepository.findAll();
+        return recommendations;
+    }
+
+    @ApiOperation(value = "Get a single recommendation")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("")
+    public Recommendation getById(
+            @ApiParam("id") @RequestParam Long id) {
+        Recommendation recommendation = recommendationRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Recommendation.class, id));
+
+        return recommendation;
+    }
+}
