8cdfc1c556c82a1542af4d5e8fcdbc80b5cdf207
Alexander Robles <alexanderrobles@umail.ucsb.edu>
2022-10-28 14:23:16 -0700

    ar - added postRecommendation

--- src/main/java/edu/ucsb/cs156/example/controllers/RecommendationController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/RecommendationController.java
@@ -31,4 +31,33 @@ public class RecommendationController extends ApiController {
         Iterable<Recommendation> recommendations = recommendationRepository.findAll();
         return recommendations;
     }
+
+public Recommendation postRecommendation(
+    @ApiParam("id") @RequestParam Long id,
+    @ApiParam("requesterEmail") @RequestParam String requesterEmail,
+    @ApiParam("professorEmail") @RequestParam String professorEmail,
+    @ApiParam("explination") @RequestParam String explination,
+    @ApiParam("done") @RequestParam boolean done,
+    @ApiParam("dateRequested (in iso format, e.g. YYYY-mm-dd; see https://en.wikipedia.org/wiki/ISO_8601)") @RequestParam("localDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateRequested,
+    @ApiParam("date (in iso format, e.g. YYYY-mm-dd; see https://en.wikipedia.org/wiki/ISO_8601)") @RequestParam("localDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateNeeded)
+        
+        throws JsonProcessingException {
+
+    // For an explanation of @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    // See: https://www.baeldung.com/spring-date-parameters
+
+    edu.ucsb.cs156.example.entities.Recommendation recommendation = new Recommendation();
+        recommendation.setID(id);
+        recommendation.setRequesterEmail(requesterEmail);
+        recommendation.setProfessorEmail(professorEmail);
+        recommendation.setExplination(explination);
+        recommendation.setDone(done);
+        recommendation.setDateRequested(dateRequested);
+        recommendation.setDateNeeded(dateNeeded);
+
+        Recommendation saveRecommendation = recommendationRepository.save(recommendation);
+
+        return recommendation;
+    }
+
 }
